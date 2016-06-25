79dde4f40ad4f229b9b878f44a56417bbe66f355
benrhanson <benrhanson@Benjamins-MacBook-Pro.local>
2016-06-24 21:40:02 -0700

    fixed root url and links

--- jimblog/urls.py
+++ jimblog/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^home/', include('apps.blogposts.urls')),
+    url(r'^', include('apps.blogposts.urls')),
     url(r'^admin/', admin.site.urls),
 ]
