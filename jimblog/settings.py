b15c2ed94838e09372d3adc1a4a10a6addf54e79
benrhanson <benrhanson@live.com>
2016-07-27 12:11:05 -0700

    sending

--- jimblog/settings.py
+++ jimblog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'jimblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jimblog',                     
+        'USER': 'user',
+        'PASSWORD': 'Stinger01!',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',
     }
 }
 
